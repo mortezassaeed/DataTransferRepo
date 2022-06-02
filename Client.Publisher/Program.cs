58ef5532479e1779f4f1e00556f5e0866eb2d8d7
mortezassaeed@gmail.com <mortezassaeed@gmail.com>
2022-06-02 22:54:23 +0430

    add publisher project

---
+++ Client.Publisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+using StackExchange.Redis;
+
+ConsoleMenuHelper.Builder consoleMenuBuilder = new ConsoleMenuHelper.Builder();
+FileHelper helper = new FileHelper();
+
+var targetDirectory = @"D:\temp";
+
+Console.WriteLine($"Select one of the following files for processing \nWe display files inside [{targetDirectory}] directory [Move between files with arrow keys]");
+var allAvalibleXlsxFlie = helper.GetAvalibleFiles(targetDirectory, "xlsx");
+var consoleHelper = consoleMenuBuilder
+    .AddX(0)
+    .AddY(2)
+    .AddOptionPerLine(1)
+    .AddSpacingPerLine(10)
+    .AddOptions(allAvalibleXlsxFlie.ToArray())
+    .ToConsoleMenueHelper();
+
+var selectedFileDirectory = consoleHelper.MultipleChoice(true);
+string connectionString = "127.0.0.1:6379";
+
+using (ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(connectionString))
+{
+    ISubscriber sub = redis.GetSubscriber();
+    helper.ReadExcle(selectedFileDirectory, (value) =>
+    {
+        if (value is not null)
+            sub.Publish("data", value);
+    });
+
+}
+
+Console.ReadKey();
+
+
+
+
+
+
